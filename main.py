5d7b8ded0bbf0c20664e0a454de21c35b9edd5a0
Andrey Kan <ripperoni@Andreys-MacBook-Pro.local>
2022-05-04 02:48:18 +0300

    optimization + ref materials

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ from collections.abc import Iterable   # import directly from collections for Py
 nltk.download('stopwords')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
+from PIL import Image
+
+
 
 
 def convertStringToDataFrame(str):
@@ -24,9 +27,6 @@ def convertStringToDataFrame(str):
     preData = StringIO(newStr)
     newData = pd.read_csv(preData, sep = "////////")
     return newData
-
-
-
 
 # Функция для очистки пунктуации
 def remove_punc(text):
@@ -51,7 +51,7 @@ def lemmatizing(tokenized_text):
     return lemma
 
 
-@st.cache(suppress_st_warning=True)
+# @st.cache(suppress_st_warning=True)
 def prepareData(data):
     data_out = data.copy()
     data_out['no_punc'] = data_out['review'].apply(lambda z: remove_punc(z))
@@ -64,63 +64,68 @@ def prepareData(data):
     # Этот шаг выполняется здесь, потому что столбец «lemmatized» представляет собой список токенизированных слов, и когда мы применяем векторизацию
     # методы, такие как count vectorizer или TFIDF, требуют ввода строки. Следовательно, преобразуем все токенизированные слова в строку
     data_out['lemmatized'] = [" ".join(review) for review in data_out['lemmatized'].values]
-    data_out.head()
+    # data_out.to_csv("preparedData.csv")
     return data_out
 
+# @st.cache(suppress_st_warning=True)
+# def readPreparedData():
+#     loaded_data = pd.read_csv('preparedData.csv', nrows = 1000)
+#     return loaded_data
 
 
 
 # Отрисовка ROC-кривой
-def draw_roc_curve(y_true, y_score, ax, pos_label=1, average='micro'):
-    fpr, tpr, thresholds = roc_curve(y_true, y_score,
-                                     pos_label=pos_label)
-    roc_auc_value = roc_auc_score(y_true, y_score, average=average)
-    lw = 2
-    ax.plot(fpr, tpr, color='darkorange',
-             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc_value)
-    ax.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-    ax.set_xlim([0.0, 1.0])
-    ax.set_xlim([0.0, 1.05])
-    ax.set_xlabel('False Positive Rate')
-    ax.set_ylabel('True Positive Rate')
-    ax.set_title('Receiver operating characteristic')
-    ax.legend(loc="lower right")
+# def draw_roc_curve(y_true, y_score, ax, pos_label=1, average='micro'):
+#     fpr, tpr, thresholds = roc_curve(y_true, y_score,
+#                                      pos_label=pos_label)
+#     roc_auc_value = roc_auc_score(y_true, y_score, average=average)
+#     lw = 2
+#     ax.plot(fpr, tpr, color='darkorange',
+#              lw=lw, label='ROC curve (area = %0.2f)' % roc_auc_value)
+#     ax.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+#     ax.set_xlim([0.0, 1.0])
+#     ax.set_xlim([0.0, 1.05])
+#     ax.set_xlabel('False Positive Rate')
+#     ax.set_ylabel('True Positive Rate')
+#     ax.set_title('Receiver operating characteristic')
+#     ax.legend(loc="lower right")
 
 
 # @st.cache(suppress_st_warning=True)
-def print_models(models_select, x_test, y_test):
-    current_models_list = []
-    scores = {}
-    for model_name in models_select:
-        model = clas_models[model_name]
-        # Предсказание значений
-        Y_pred = model.predict(x_test)
-        # Предсказание вероятности класса "1" для roc auc
-        Y_pred_proba_temp = model.predict_proba(x_test)
-        Y_pred_proba = Y_pred_proba_temp[:, 1]
+# def print_models(models_select, x_test, y_test):
+#     current_models_list = []
+#     scores = pd.DataFrame(index=[0])
+#     for model_name in models_select:
+#         model = clas_models[model_name]
+#         # Предсказание значений
+#         Y_pred = model.predict(x_test)
+#         # Предсказание вероятности класса "1" для roc auc
+#         Y_pred_proba_temp = model.predict_proba(x_test)
+#         Y_pred_proba = Y_pred_proba_temp[:, 1]
+#
+#         current_models_list.append(model_name)
+#         scores["accuracy"] = accuracy_score(y_test, Y_pred)
+#         scores["precision"] = precision_score(y_test, Y_pred)
+#         scores["recall"] = recall_score(y_test, Y_pred)
+#         scores["f1 score"] = f1_score(y_test, Y_pred)
+#         # scores.to_csv(model_name+".csv")
+#
+#         # scores = pd.DataFrame(data = scores, index=[0])
+#         # Отрисовка ROC-кривых
+#         fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
+#         draw_roc_curve(y_test.values, Y_pred_proba, ax[0])
+#
+#         plot_confusion_matrix(model, x_test, y_test.values, ax=ax[1],
+#                               display_labels=['0', '1'],
+#                               cmap=plt.cm.Blues, normalize='true')
+#         fig.suptitle(model_name)
+#         graphCol.pyplot(fig)
+#         graphCol.write(scores)
 
-        current_models_list.append(model_name)
-        scores["accuracy"] = accuracy_score(y_test, Y_pred)
-        scores["precision"] = precision_score(y_test, Y_pred)
-        scores["recall"] = recall_score(y_test, Y_pred)
-        scores["f1 score"] = f1_score(y_test, Y_pred)
-
-        scores = pd.DataFrame(data = scores, index=[0])
-        # Отрисовка ROC-кривых
-        fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
-        draw_roc_curve(y_test.values, Y_pred_proba, ax[0])
-
-        plot_confusion_matrix(model, x_test, y_test.values, ax=ax[1],
-                              display_labels=['0', '1'],
-                              cmap=plt.cm.Blues, normalize='true')
-        fig.suptitle(model_name)
-        graphCol.pyplot(fig)
-        graphCol.write(scores)
-
-@st.cache(suppress_st_warning=True)
-def load_data():
-    data = pd.read_csv('movie_data.csv', nrows = 2000)
-    return data
+# @st.cache(suppress_st_warning=True)
+# def load_data():
+#     data = pd.read_csv('movie_data.csv', nrows = 500)
+#     return data
 
 def clear_text():
     st.session_state["text"] = ""
@@ -128,6 +133,37 @@ def clear_text():
 @st.cache(suppress_st_warning=True, allow_output_mutation= True)
 def load_model(model):
     return pickle.load(open(model, 'rb'))
+
+
+# @st.cache(suppress_st_warning=True, allow_output_mutation=True)
+# def separate_data():
+#     # Разделение данных на более мелкие кадры данных для тестирования
+#     x_test = data.iloc[:, 5]
+#     y_test = data.iloc[:, 1]
+#     x_test = tfidf_vect.transform(x_test.values)
+#     return x_test, y_test
+
+
+
+class Model:
+    def __init__(self, name, scores, image):
+        self.name = name
+        self.scores = scores
+        self.image = image
+
+@st.cache(suppress_st_warning=True)
+def init_models():
+    models_array = []
+    for model_name in models_list:
+        models_array.append(Model(model_name, pd.read_csv(model_name+".csv", index_col=[0]), Image.open(model_name+".png")))
+    return models_array
+
+def print_static_models():
+    for model_name in models_select:
+        for model in models:
+            if model_name == model.name:
+                graphCol.image(model.image)
+                graphCol.write(model.scores)
 
 
 
@@ -155,16 +191,14 @@ lg_model = load_model("Lg_reg_tf.sav")
 bar.progress(0.5)
 gb_model = load_model("GB_tf.sav")
 bar.progress(0.625)
-data = load_data()
+# data = load_data()
 bar.progress(0.75)
-data = prepareData(data)
+# data = prepareData(data)
+# data = readPreparedData()
 bar.progress(0.875)
 
 
-# Разделение данных на более мелкие кадры данных для тестирования
-x_test = data.iloc[:, 5]
-y_test = data.iloc[:, 1]
-x_test = tfidf_vect.transform(x_test.values)
+# x_test, y_test = separate_data()
 bar.progress(1.0)
 data_load_state.text("")
 bar.empty()
@@ -172,20 +206,44 @@ bar.empty()
 
 
 # Модели
-models_list = ['Bernoulli Naive Bayes', 'Multinomial Naive Bayes', 'Logistic Regression', 'Gradient Boosting']
+models_list = ['Multinomial Naive Bayes', 'Bernoulli Naive Bayes', 'Logistic Regression', 'Gradient Boosting']
 
-clas_models = {'Bernoulli Naive Bayes': ber_nb_model,
-                  'Multinomial Naive Bayes': best_model,
-                  'Logistic Regression': lg_model,
-                  'Gradient Boosting': gb_model}
+clas_models = {'Multinomial Naive Bayes': best_model,
+               'Bernoulli Naive Bayes': ber_nb_model,
+               'Logistic Regression': lg_model,
+               'Gradient Boosting': gb_model}
+models = init_models()
+st.subheader("Справочные материалы")
+with st.expander("ROC-кривая"):
+    st.write("""***ROC-кривая*** — график, позволяющий оценить качество бинарной классификации, отображает соотношение между долей объектов от общего количества носителей признака, верно классифицированных как несущие признак (англ. true positive rate, TPR, называемой чувствительностью алгоритма классификации), и долей объектов от общего количества объектов, не несущих признака, ошибочно классифицированных как несущие признак (англ. false positive rate).""")
+    st.write("""Количественная интерпретация ROC даёт показатель AUC (англ. Area Under Curve, площадь под кривой) — площадь, ограниченная ROC-кривой и осью доли ложных положительных классификаций. Чем выше показатель AUC, тем качественнее классификатор, при этом значение 0,5 демонстрирует непригодность выбранного метода классификации (соответствует случайному гаданию).""")
+with st.expander("Матрица ошибок (Confusion Matrix) и метрики"):
+    st.write("""***Истинно позитивное предсказание (True Positive, сокр. TP)***\n
+Вы предсказали положительный результат, и результат действительно положительный.""")
+    st.write("""***Истинно отрицательное предсказание (True Negative, TN)***\n
+Вы предсказали отрицательный результат, и результат действительно отрицательный.""")
+    st.write("""***Ошибочно положительное предсказание (False Positive, FN)***\n
+Вы предсказали положительный результат, но на самом деле это не так.""")
+    st.write("""***Ошибочно отрицательное предсказание (False Negative, FN)***\n
+Вы предсказали отрицательный результат, но на самом деле это не так.""")
+    st.image("Confusion matrix.png")
+    st.markdown(r'''Accuracy = $$ \frac{TP+TN}{TP+FP+FN+TN}$$''')
+    st.markdown(r'''Precision = $$ \frac{TP}{TP+FP}$$''')
+    st.markdown(r'''Recall = $$ \frac{TP}{TP+FN}$$''')
+    st.markdown(r'''F1 score = $$ 2\frac{Recall * Precision}{Recall + Precision}$$''')
+
 
 graphCol, predictCol = st.columns([3, 1])
 graphCol.subheader('Оценка качества моделей')
 models_select = graphCol.multiselect('Выберите модели', models_list)
 
-print_models(models_select, x_test, y_test)
+
+
+# print_models(models_select, x_test, y_test)
+print_static_models()
 
 predictCol.subheader("Классификация отзывов")
+radioButton = predictCol.radio("Выберите модель", models_list, 0)
 txt = predictCol.text_area(label = "Отзыв для классификации", key="text", height=300)
 predictCol.button("Очистить", on_click=clear_text)
 if predictCol.button("Классифицировать"):
@@ -193,7 +251,7 @@ if predictCol.button("Классифицировать"):
         review = convertStringToDataFrame(txt)
         data_text = prepareData(review)
         data_text = tfidf_vect.transform(data_text['lemmatized'].values)
-        result = best_model.predict(data_text)
+        result = clas_models[radioButton].predict(data_text)
         if result[0] == 1:
             predictCol.success("Положительный отзыв")
         else:
