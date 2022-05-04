655869fd90d55ea6752ee025911a990f25507b95
Andrey Kan <ripperoni@Andreys-MacBook-Pro.local>
2022-05-04 20:17:49 +0300

    deleted screenshots

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def lemmatizing(tokenized_text):
     return lemma
 
 
-# @st.cache(suppress_st_warning=True)
+@st.cache(suppress_st_warning=True)
 def prepareData(data):
     data_out = data.copy()
     data_out['no_punc'] = data_out['review'].apply(lambda z: remove_punc(z))
@@ -75,57 +75,57 @@ def prepareData(data):
 
 
 # Отрисовка ROC-кривой
-# def draw_roc_curve(y_true, y_score, ax, pos_label=1, average='micro'):
-#     fpr, tpr, thresholds = roc_curve(y_true, y_score,
-#                                      pos_label=pos_label)
-#     roc_auc_value = roc_auc_score(y_true, y_score, average=average)
-#     lw = 2
-#     ax.plot(fpr, tpr, color='darkorange',
-#              lw=lw, label='ROC curve (area = %0.2f)' % roc_auc_value)
-#     ax.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-#     ax.set_xlim([0.0, 1.0])
-#     ax.set_xlim([0.0, 1.05])
-#     ax.set_xlabel('False Positive Rate')
-#     ax.set_ylabel('True Positive Rate')
-#     ax.set_title('Receiver operating characteristic')
-#     ax.legend(loc="lower right")
+def draw_roc_curve(y_true, y_score, ax, pos_label=1, average='micro'):
+    fpr, tpr, thresholds = roc_curve(y_true, y_score,
+                                     pos_label=pos_label)
+    roc_auc_value = roc_auc_score(y_true, y_score, average=average)
+    lw = 2
+    ax.plot(fpr, tpr, color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc_value)
+    ax.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    ax.set_xlim([0.0, 1.0])
+    ax.set_xlim([0.0, 1.05])
+    ax.set_xlabel('False Positive Rate')
+    ax.set_ylabel('True Positive Rate')
+    ax.set_title('Receiver operating characteristic')
+    ax.legend(loc="lower right")
 
 
 # @st.cache(suppress_st_warning=True)
-# def print_models(models_select, x_test, y_test):
-#     current_models_list = []
-#     scores = pd.DataFrame(index=[0])
-#     for model_name in models_select:
-#         model = clas_models[model_name]
-#         # Предсказание значений
-#         Y_pred = model.predict(x_test)
-#         # Предсказание вероятности класса "1" для roc auc
-#         Y_pred_proba_temp = model.predict_proba(x_test)
-#         Y_pred_proba = Y_pred_proba_temp[:, 1]
-#
-#         current_models_list.append(model_name)
-#         scores["accuracy"] = accuracy_score(y_test, Y_pred)
-#         scores["precision"] = precision_score(y_test, Y_pred)
-#         scores["recall"] = recall_score(y_test, Y_pred)
-#         scores["f1 score"] = f1_score(y_test, Y_pred)
-#         # scores.to_csv(model_name+".csv")
-#
-#         # scores = pd.DataFrame(data = scores, index=[0])
-#         # Отрисовка ROC-кривых
-#         fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
-#         draw_roc_curve(y_test.values, Y_pred_proba, ax[0])
-#
-#         plot_confusion_matrix(model, x_test, y_test.values, ax=ax[1],
-#                               display_labels=['0', '1'],
-#                               cmap=plt.cm.Blues, normalize='true')
-#         fig.suptitle(model_name)
-#         graphCol.pyplot(fig)
-#         graphCol.write(scores)
+def print_models(models_select, x_test, y_test):
+    current_models_list = []
+    scores = pd.DataFrame(index=[0])
+    for model_name in models_select:
+        model = clas_models[model_name]
+        # Предсказание значений
+        Y_pred = model.predict(x_test)
+        # Предсказание вероятности класса "1" для roc auc
+        Y_pred_proba_temp = model.predict_proba(x_test)
+        Y_pred_proba = Y_pred_proba_temp[:, 1]
 
-# @st.cache(suppress_st_warning=True)
-# def load_data():
-#     data = pd.read_csv('movie_data.csv', nrows = 500)
-#     return data
+        current_models_list.append(model_name)
+        scores["accuracy"] = accuracy_score(y_test, Y_pred)
+        scores["precision"] = precision_score(y_test, Y_pred)
+        scores["recall"] = recall_score(y_test, Y_pred)
+        scores["f1 score"] = f1_score(y_test, Y_pred)
+        # scores.to_csv(model_name+".csv")
+
+        # scores = pd.DataFrame(data = scores, index=[0])
+        # Отрисовка ROC-кривых
+        fig, ax = plt.subplots(ncols=2, figsize=(10, 5))
+        draw_roc_curve(y_test.values, Y_pred_proba, ax[0])
+
+        plot_confusion_matrix(model, x_test, y_test.values, ax=ax[1],
+                              display_labels=['0', '1'],
+                              cmap=plt.cm.Blues, normalize='true')
+        fig.suptitle(model_name)
+        graphCol.pyplot(fig)
+        graphCol.write(scores)
+
+@st.cache(suppress_st_warning=True)
+def load_data(rows):
+    data = pd.read_csv('movie_data.csv', nrows = rows)
+    return data
 
 def clear_text():
     st.session_state["text"] = ""
@@ -135,35 +135,14 @@ def load_model(model):
     return pickle.load(open(model, 'rb'))
 
 
-# @st.cache(suppress_st_warning=True, allow_output_mutation=True)
-# def separate_data():
-#     # Разделение данных на более мелкие кадры данных для тестирования
-#     x_test = data.iloc[:, 5]
-#     y_test = data.iloc[:, 1]
-#     x_test = tfidf_vect.transform(x_test.values)
-#     return x_test, y_test
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def separate_data():
+    # Разделение данных на более мелкие кадры данных для тестирования
+    x_test = data.iloc[:, 5]
+    y_test = data.iloc[:, 1]
+    x_test = tfidf_vect.transform(x_test.values)
+    return x_test, y_test
 
-
-
-class Model:
-    def __init__(self, name, scores, image):
-        self.name = name
-        self.scores = scores
-        self.image = image
-
-@st.cache(suppress_st_warning=True)
-def init_models():
-    models_array = []
-    for model_name in models_list:
-        models_array.append(Model(model_name, pd.read_csv(model_name+".csv", index_col=[0]), Image.open(model_name+".png")))
-    return models_array
-
-def print_static_models():
-    for model_name in models_select:
-        for model in models:
-            if model_name == model.name:
-                graphCol.image(model.image)
-                graphCol.write(model.scores)
 
 
 
@@ -191,14 +170,15 @@ lg_model = load_model("Lg_reg_tf.sav")
 bar.progress(0.5)
 gb_model = load_model("GB_tf.sav")
 bar.progress(0.625)
-# data = load_data()
+n_rows = st.slider("Выберите количество строк в датасете", 200, 50000, 1000, 200)
+data = load_data(n_rows)
 bar.progress(0.75)
-# data = prepareData(data)
+data = prepareData(data)
 # data = readPreparedData()
 bar.progress(0.875)
 
 
-# x_test, y_test = separate_data()
+x_test, y_test = separate_data()
 bar.progress(1.0)
 data_load_state.text("")
 bar.empty()
@@ -212,7 +192,7 @@ clas_models = {'Multinomial Naive Bayes': best_model,
                'Bernoulli Naive Bayes': ber_nb_model,
                'Logistic Regression': lg_model,
                'Gradient Boosting': gb_model}
-models = init_models()
+# models = init_models()
 st.subheader("Справочные материалы")
 with st.expander("ROC-кривая"):
     st.write("""***ROC-кривая*** — график, позволяющий оценить качество бинарной классификации, отображает соотношение между долей объектов от общего количества носителей признака, верно классифицированных как несущие признак (англ. true positive rate, TPR, называемой чувствительностью алгоритма классификации), и долей объектов от общего количества объектов, не несущих признака, ошибочно классифицированных как несущие признак (англ. false positive rate).""")
@@ -239,8 +219,7 @@ models_select = graphCol.multiselect('Выберите модели', models_lis
 
 
 
-# print_models(models_select, x_test, y_test)
-print_static_models()
+print_models(models_select, x_test, y_test)
 
 predictCol.subheader("Классификация отзывов")
 radioButton = predictCol.radio("Выберите модель", models_list, 0)
